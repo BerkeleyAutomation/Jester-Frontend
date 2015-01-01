d01467f057f1efea15d50dd6bcf364cdb82eab6b
Viraj Mahesh <virajmahesh@gmail.com>
2015-01-01 12:46:26 +0400

    Adding register and login pages

--- js/script.js
+++ js/script.js
@@ -3,5 +3,14 @@
  */
 
 $(document).ready(function() {
-
+    console.log("Ready");
+    $('.input-field').keyup(function(event) {
+        id = event.target.id;
+        if ($('#' + id).val() != '') {
+            $('#' + id + '-backtext').hide();
+        }
+        else {
+            $('#' + id + '-backtext').show();
+        }
+    });
 });
