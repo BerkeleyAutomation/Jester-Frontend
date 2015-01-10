52988adcc3c3704e276b4a40448064cccf8859c0
Viraj Mahesh <virajmahesh@gmail.com>
2015-01-10 21:40:31 +0400

    Navbar dropdown is dark in color (visible on mobile devices)

--- js/script.js
+++ js/script.js
@@ -2,6 +2,20 @@
  * @author: Viraj Mahesh
  */
 
-$(document).ready(function() {
+function setNavbarDropbownBackground() {
+    if ($(window).width() <= 768) {
+        $('#navbar-collapse').css('background', 'rgb(195, 85, 85)');
+    }
+    else {
+        $('#navbar-collapse').css('background', 'rgb(240, 100, 100)');
+    }
+}
 
+$(document).ready(function() {
+    // Set Background on page load
+    setNavbarDropbownBackground();
+    // Window is resized after loading
+    $(window).resize(function() {
+        setNavbarDropbownBackground();
+    });
 });
