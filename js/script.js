072a25b76b6f907fa6907668de239a3499f83b8e
Viraj Mahesh <virajmahesh@gmail.com>
2015-01-10 21:08:45 +0400

    Updated author tag and removed old JS

--- js/script.js
+++ js/script.js
@@ -1,16 +1,7 @@
 /**
- * Created by Viraj.
+ * @author: Viraj Mahesh
  */
 
 $(document).ready(function() {
-    console.log("Ready");
-    $('.input-field').keyup(function(event) {
-        id = event.target.id;
-        if ($('#' + id).val() != '') {
-            $('#' + id + '-backtext').hide();
-        }
-        else {
-            $('#' + id + '-backtext').show();
-        }
-    });
+
 });
