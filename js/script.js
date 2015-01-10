366f3a099a9afa72cd7316c6dbad9673963ca7da
Viraj Mahesh <virajmahesh@gmail.com>
2015-01-11 01:10:32 +0400

    Added code to justify Navbar contents - Justifies Navbar contents by making them equally sized

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
  * @author: Viraj Mahesh
  */
 
+/**
+ * Sets the Navbar drop-down color as dark red.
+ */
 function setNavbarDropbownBackground() {
     if ($(window).width() <= 768) {
         $('#navbar-collapse').css('background', 'rgb(195, 85, 85)');
@@ -11,11 +14,30 @@ function setNavbarDropbownBackground() {
     }
 }
 
+/**
+ * Equal spaces the items on the Navbar if the screen is large
+ * Otherwise, Navbar items occupy the entire line in the dropdown menu
+ */
+function justifyNavbar() {
+    if ($(window).width() > 768) {
+        var items = $('.item').length; // Number of Navbar items
+        var navbarWidth = $('.collapse').width(); // Navbar width
+        // Equal sized items. Offset by -1 for smooth resizing
+        var itemWidth = (navbarWidth/items) - 1.0;
+        $('.item').width(itemWidth);
+    }
+    else {
+        $('.item').css('width', '100%'); // Navbar items occupy entire line
+    }
+}
+
 $(document).ready(function() {
     // Set Background on page load
     setNavbarDropbownBackground();
+    justifyNavbar();
     // Window is resized after loading
     $(window).resize(function() {
         setNavbarDropbownBackground();
+        justifyNavbar();
     });
 });
