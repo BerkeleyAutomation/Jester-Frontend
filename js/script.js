adbf84d4f3fdb600d658ad59d473db8ef8015528
Viraj Mahesh <virajmahesh@gmail.com>
2015-02-24 19:47:31 -0800

    Fixed bugs and auth issues - Initial setup is faster using angular-js promise

--- js/script.js
+++ js/script.js
@@ -2,12 +2,23 @@
  * @author: Viraj Mahesh
  */
 
+
+BASE_URL = 'http://automation.berkeley.edu/jester_backend/jester/';
+REQUEST_URL = BASE_URL + 'request_joke/';
+RATE_URL = BASE_URL + 'rate_joke/{0}/{1}/';
+
+String.prototype.format = String.prototype.f = function () {
+    var s = this,
+        i = arguments.length;
+    while (i--) {
+        s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+    }
+    return s;
+};
+
 /**
- * Sets the Navbar drop-down color as dark red.
+ * Set the Navbar dropdown color as dark red
  */
-
-BASE_URL = 'http://automation.berkeley.edu/jester_backend/jester/'
-
 function setNavbarDropbownBackground() {
     if ($(window).width() < 768) {
         $('#navbar-collapse').css('background', 'rgb(195, 85, 85)');
@@ -23,23 +34,28 @@ function setNavbarDropbownBackground() {
  */
 function justifyNavbar() {
     if ($(window).width() >= 768) {
-        var items = $('.item').length; // Number of Navbar items
+        var items = $('.item'); // Number of Navbar items
         var navbarWidth = $('.collapse').width(); // Navbar width
         // Equal sized items. Offset by -1 for smooth resizing
-        var itemWidth = (navbarWidth / items) - 1.0;
-        $('.item').width(itemWidth);
+        var itemWidth = (navbarWidth / items.length) - 1.0;
+        items.width(itemWidth);
     }
     else {
         $('.item').css('width', '100%'); // Navbar items occupy entire line
     }
 }
 
+/**
+ * Controller for the register dialog
+ * @param $scope
+ * @param $mdDialog
+ */
 function RegisterController($scope, $mdDialog) {
     $scope.cancel = function () {
         $mdDialog.cancel();
         console.log('Register cancel');
     };
-    $scope.submit = function() {
+    $scope.submit = function () {
         if (typeof $scope.user.email != 'undefined') {
             var c = get(BASE_URL + 'register_user/' + $scope.user.email + '/' +
             $scope.user.password + '/');
@@ -48,6 +64,12 @@ function RegisterController($scope, $mdDialog) {
     };
 }
 
+/**
+ * Controller for the login dialog
+ * @param $scope
+ * @param $mdDialog
+ * @constructor
+ */
 function LoginController($scope, $mdDialog) {
     $scope.cancel = function () {
         $mdDialog.cancel();
@@ -55,6 +77,9 @@ function LoginController($scope, $mdDialog) {
     };
 }
 
+/**
+ * Run this code when the page loads
+ */
 $(document).ready(function () {
     setNavbarDropbownBackground();
     justifyNavbar();
@@ -65,12 +90,24 @@ $(document).ready(function () {
     });
 });
 
-function requestJoke($scope) {
-    $.get(BASE_URL + 'request_joke/', function(data) {
-        $scope.joke = data;
-        $scope.$apply();
+/**
+ * Requests the next joke from the server. Uses $http to refresh the view as soon
+ * as joke is available.
+ * @param $scope The angular-js scope to bind the joke data.
+ * @param $http
+ */
+function requestJoke($scope, $http) {
+    var promise = $http({
+        url: REQUEST_URL,
+        type: 'GET',
+        withCredentials: true
+    });
+    // Update the view once a response is received
+    promise.then(function (payload) {
+        $scope.joke = payload.data;
     });
 }
+
 
 angular.module('jester', ['ngMaterial'])
     .controller('navbar-controller', function ($scope, $mdDialog) {
@@ -89,24 +126,28 @@ angular.module('jester', ['ngMaterial'])
             });
         };
     })
-    .controller('joke-controller', function($scope) {
+    .controller('joke-controller', function ($scope) {
         // Set default rating
         $scope.rating = 0;
-        // Request a new joke. This will perform authentication
+        // Request a new joke. This will also perform authentication
         requestJoke($scope);
         // Submit a rating and request the next joke
-        $scope.submitRating = function (event) {
-            var url = BASE_URL + 'rate_joke/' + $scope.joke.joke_id +
-                '/' + $scope.rating.toFixed(2) + '/';
-            $.get(url, function() {
-                requestJoke($scope);
-            })
+        $scope.submitRating = function () {
+            // Submit the rating
+            var promise = $http({
+                url: RATE_URL.format($scope.joke.joke_id, $scope.rating.toFixed(3)),
+                type: 'GET',
+                withCredentials: true
+            });
+            // Request a new joke once a response is received
+            promise.then(function() {
+                requestJoke($scope, $http);
+            });
         };
     })
     // Directly inject html
-    .filter('unsafe', function($sce) {
-        return function(val) {
+    .filter('unsafe', function ($sce) {
+        return function (val) {
             return $sce.trustAsHtml(val);
         };
     });
-;
